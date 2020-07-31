8adbc8f300387cbba5b693ebdde0ce4300bfa176
TsaiKoga <969024089@qq.com>
2020-07-31 14:32:11 +0800

    [Fix] Escape double quotes in double quotes for bindings

--- src/Query/QueryLog.php
+++ src/Query/QueryLog.php
@@ -185,7 +185,7 @@ class QueryLog
     {
         $bindings = array_map(function($q) {
                         $result = $q;
-                        if (is_string($q) && !in_array(mb_strtolower($q), ["true", "false"])) $result = "\"$q\"";
+                        if (is_string($q) && !in_array(mb_strtolower($q), ["true", "false"])) $result = "\"".str_replace('"', '\"', $q)."\"";
                         if (in_array(mb_strtolower($q), ["true", "false"])) $result = mb_strtolower($q);
                         if (is_null($q)) $result = "null";
                         if (is_object($q) && (new \ReflectionClass($q))->getShortName() === "Carbon") $result = "\"{$q->format('Y-m-d H:i:s')}\"";
